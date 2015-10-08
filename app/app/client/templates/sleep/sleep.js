dbc6255b0ec57a699bee2c8047d328ab8587021e
Sun Wang Jun <sunxperous@gmail.com>
2015-10-09 01:42:47 +0800

    add reactivity to sleep chart

--- app/app/client/templates/sleep/sleep.js
+++ app/app/client/templates/sleep/sleep.js
@@ -7,10 +7,11 @@ Template.Sleep.events({
 /*****************************************************************************/
 /* Sleep: Helpers */
 /*****************************************************************************/
-var data = [3, 2, 2, 1, 2, 1, 1, 1, 3, 3];
+var move = new ReactiveVar([3, 2, 2, 1, 2, 1, 1, 1, 3, 3, 1, 2, 3, 1, 2, 3, 3, 2, 1]);
 Template.Sleep.helpers({
-  data: function() {
-    return data;
+  move: function() {
+    console.log(move.get());
+    return move.get();
   }
 });
 
@@ -18,21 +19,37 @@ Template.Sleep.helpers({
 /* Sleep: Lifecycle Hooks */
 /*****************************************************************************/
 Template.Sleep.onCreated(function () {
+  move.set([1, 2, 3]);
+  setTimeout(function() {
+    move.set([3, 2, 1, 3, 2, 1]);
+  }, 1000);
 });
 
+var colors = ['black', 'red', 'orange', 'green'];
 Template.Sleep.onRendered(function () {
-  var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 400]);
-  var chart = d3.select('.sleep')
-    .attr('width', 400)
-    .attr('height', 400);
-  var bar = chart.selectAll('g')
-    .data(data)
-    .enter().append('g')
-      .attr('transform', function(d, i) { return 'translate(' + i * 20 + ', 0)'; });
-  bar.append('rect')
-    .attr('y', function(d) { return 400 - x(d); })
-    .attr('width', 20)
-    .attr('height', function(d) { return x(d); });
+  var height = 400;
+  Tracker.autorun(function() {
+    data = move.get();
+
+    // Format chart measurements.
+    var x = d3.scale.linear().domain([0, d3.max(data)]).range([0, height]);
+    var chart = d3.select('.sleep')
+      .attr('width', '90%')
+      .attr('height', height);
+
+    // Add new bars for each new data.
+    var barNew = chart.selectAll('g').data(data).enter().append('g');
+    barNew.append('rect');
+
+    // Format bars for existing and new data together.
+    var bar = chart.selectAll('g');
+    bar.select('rect')
+      .attr('fill', function(d) { return colors[d]; })
+      .attr('x', function(d, i) { return (100 / data.length * i) + '%'; })
+      .attr('y', function(d) { return height - x(d); })
+      .attr('width', (99 / data.length) + '%')
+      .attr('height', function(d) { return x(d); });
+  });
 });
 
 Template.Sleep.onDestroyed(function () {
