116e9234d396f5e6184a95c89c96f68a07307a99
Kaikj <bluesword246@gmail.com>
2015-10-09 23:09:34 +0800

    Add sleep levels from the turbulence data

--- app/app/server/bootstrap.js
+++ app/app/server/bootstrap.js
@@ -13,5 +13,42 @@ if (Meteor.isServer) {
         }
       });
     }, 333);
+
+    var count = 1;
+    var query = Turbulence.find();
+    var handle = query.observe({
+      addedAt: function (doc, id, before) {
+        count++
+        count = (count % 15);
+        if (count === 0) {
+          var disturbance = 0;
+          var cur = Turbulence.find({}, {
+            skip: (id - 14),
+            limit: 15
+          });
+          cur.forEach(function (tur) {
+            if (tur.magnitude !== 0) {
+              disturbance++;
+            }
+          });
+          if (disturbance <= 0) {
+            Sleep.insert({
+              'level': 1,
+              'timestamp': cur.fetch()[0].timestamp
+            });
+          } else if (disturbance > 0 && disturbance < 11) {
+            Sleep.insert({
+              'level': 2,
+              'timestamp': cur.fetch()[0].timestamp
+            });
+          } else {
+            Sleep.insert({
+              'level': 3,
+              'timestamp': cur.fetch()[0].timestamp
+            });
+          }
+        }
+      }
+    });
   })
 }
