546e0098391f1fd61239b760edfcbbf2ed32d31b
Kaikj <bluesword246@gmail.com>
2015-10-09 11:54:12 +0800

    Poll device every second for turbulence data

--- app/app/server/bootstrap.js
+++ app/app/server/bootstrap.js
@@ -1,2 +1,21 @@
-Meteor.startup(function () {
-});
+if (Meteor.isServer) {
+	Meteor.startup(function () {
+		Meteor.setInterval(function () {
+			// Initiate HTTP request
+			var xmlHttp = new XMLHttpRequest();
+
+			// Function to call on GET success
+			xmlHttp.onreadystatechange = function () {
+				if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+					var result = JSON.parse(xmlHttp.responseText);
+					Turbulence.insert(result.magnitude);
+				}
+			}
+
+			// Get the info
+			xmlHttp.open("GET", "192.168.4.1", true);
+			xmlHttp.send(null);
+
+		}, 1000);
+	})
+}
